5537c06c2e530ba9f57114aa7e2af807bc69de55
jaylenpatterson <jaylenpatterson16@gmail.com>
2024-12-17 20:26:08 -0500

    Create solution for who-likes-this

---
+++ 6-Kyu/who-likes-this.js
@@ -0,0 +1,15 @@
+function likes(names) {
+    const count = names.length;
+    switch (count) {
+        case 0:
+            return "no one likes this";
+        case 1:
+            return `${names[0]} likes this`;
+        case 2:
+            return `${names[0]} and ${names[1]} like this`;
+        case 3:
+            return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+        default:
+        return `${names[0]}, ${names[1]} and ${count - 2} others like this`;
+    }
+}
